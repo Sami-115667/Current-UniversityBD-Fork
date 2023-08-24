4f8f40aa96c7119d95d696cd2f34066545b5d00b
Sami-115667 <shamsurrahman07052001@gmail.com>
2023-08-25 00:02:23 +0600

    User_Profile folder is created

---
+++ app/src/main/java/com/techtravelcoder/universitybd/user_profile/User_Profile_Activity.java
@@ -0,0 +1,16 @@
+package com.techtravelcoder.universitybd.user_profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.techtravelcoder.universitybd.R;
+
+public class User_Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
